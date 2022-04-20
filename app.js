2d26a35905c54aa95f776dd5dc8f69b9efb19a5e
kumarabhishek404 <abhishek19@navgurukul.org>
2022-04-20 18:51:34 +0530

    GET and POST request method is added

--- app.js
+++ app.js
@@ -1,33 +1,63 @@
 var express = require('express');
 var axios = require('axios')
+const cors = require('cors');
+
 
 var app = express()
 
+app.use(cors({
+    origin: '*'
+}));
+
+
 const getData = async () => {
     try {
-        return await axios.get('https://jsonplaceholder.typicode.com/posts/1/comments') 
+        return await axios.get('https://jsonplaceholder.typicode.com/posts/1/comments')
     } catch (err) {
         console.log(err)
     }
 }
 
-app.get('/', (req,res) => {
-    res.send('Hello World!!!');
+const setData = async (payload) => {
+    try {
+        return await axios.post('http://dummy.restapiexample.com/api/v1/create', payload)
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+app.get('/', (req, res) => {
+    // res.send('Hello World!!!');
     // res.setHeader('Content-Type', 'text/plain');
     getData()
-    .then((respose) => {
-        console.log("Response", respose.data);
-        return respose.data
-    })
-    .catch((err) => {
-        console.log("Error", err);
-        res.send('Failed')
-    })
+        .then((response) => {
+            console.log("Response", response.data);
+            res.send(response.data);
+        })
+        .catch((err) => {
+            console.log("Error", err);
+            res.send('Failed')
+        })
+})
+
+app.post('/add', (req, res) => {
+    console.log("Request Body", res.json({requestBody: req.body}));
+    // res.json({requestBody: req.body})
+    setData(req.body)
+        .then(response => {
+            console.log("Post Response", response.data);
+        })
+        .catch(err => {
+            console.log("Post Error", err);
+        })
+    // const add_data = req.body;
+    // console.log("Body data", add_data)
+    // res.send("New add success")
 })
 
 var server = app.listen("8080", () => {
     var host = server.address().address
     var port = server.address().port
     console.log(host);
-    console.log('App is listening at http://%s:%s', host,port)
+    console.log('App is listening at http://%s:%s', host, port)
 })
